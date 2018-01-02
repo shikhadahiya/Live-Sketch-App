0b45757d2f9d3c5ece8bc56311f73c6588afda72
shikhadahiya <shikha.dahiya25@yahoo.com>
2018-01-03 00:00:11 +0530

    Add files via upload

---
+++ liveSketch.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+import numpy as np
+
+def sketch(image):
+    img_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+    img_gray_blur = cv.GaussianBlur(img_gray, (5,5), 0)
+
+    canny_edges = cv.Canny(img_gray_blur, 10, 70)
+
+    ret, mask = cv.threshold(canny_edges, 70, 255, cv.THRESH_BINARY_INV)
+    return mask
+
+cap = cv.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    cv.imshow('Live Sketcher',sketch(frame))
+    if cv.waitKey(1) == 13:
+        break
+
+cap.release()
+cv.destroyAllWindows()
